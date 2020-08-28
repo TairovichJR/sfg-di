a9f4aa735d747baf0ed08878f8b0c4426f0ba93f
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-08-27 23:29:41 -0400

    fixes

--- src/main/java/guru/springframework/controllers/I18NController.java
+++ src/main/java/guru/springframework/controllers/I18NController.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Controller;
  * Created by jt on 12/27/19.
  */
 @Controller
-public class I18nController {
+public class I18NController {
 
     private final GreetingService greetingService;
 
-    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+    public I18NController(@Qualifier("i18nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
